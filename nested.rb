3d83c314fc24c71acbe3e04a25b717cba68226bb
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-13 20:15:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-alans_new_info = [:known_for => "GUI"]
+
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -103,7 +103,7 @@ alans_new_info = [:known_for => "GUI"]
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = ["GUI"]
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
